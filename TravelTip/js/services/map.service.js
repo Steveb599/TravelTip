d676a75874701a8a6bbd26a26b99c189d432438b
Eyal-Avni <67070652+Eyal-Avni@users.noreply.github.com>
2023-05-03 16:46:15 +0300

    layout initialization

--- TravelTip/js/services/map.service.js
+++ TravelTip/js/services/map.service.js
@@ -1,32 +1,29 @@
 export const mapService = {
     initMap,
     addMarker,
-    panTo
+    panTo,
 }
-
 
 // Var that is used throughout this Module (not global)
 var gMap
 
 function initMap(lat = 32.0749831, lng = 34.9120554) {
     console.log('InitMap')
-    return _connectGoogleApi()
-        .then(() => {
-            console.log('google available')
-            gMap = new google.maps.Map(
-                document.querySelector('#map'), {
-                center: { lat, lng },
-                zoom: 15
-            })
-            console.log('Map!', gMap)
+    return _connectGoogleApi().then(() => {
+        console.log('google available')
+        gMap = new google.maps.Map(document.querySelector('#map'), {
+            center: { lat, lng },
+            zoom: 15,
         })
+        console.log('Map!', gMap)
+    })
 }
 
 function addMarker(loc) {
     var marker = new google.maps.Marker({
         position: loc,
         map: gMap,
-        title: 'Hello World!'
+        title: 'Hello World!',
     })
     return marker
 }
@@ -36,10 +33,9 @@ function panTo(lat, lng) {
     gMap.panTo(laLatLng)
 }
 
-
 function _connectGoogleApi() {
     if (window.google) return Promise.resolve()
-    const API_KEY = '' //TODO: Enter your API Key
+    const API_KEY = 'AIzaSyDCNE344_lv4Mvr0Ne4-Qqi_lA7vxB5NVU' //TODO: Enter your API Key
     var elGoogleApi = document.createElement('script')
     elGoogleApi.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`
     elGoogleApi.async = true
